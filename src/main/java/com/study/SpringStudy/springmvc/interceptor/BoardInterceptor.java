80af8623e978c83cc2ae2c14ca17418322e3239d
ckk914 <ckk9114@naver.com>
2024-06-03 16:24:59 +0900

    # 삭제 요청시 처리 추가     - 관리자인지? 글쓴이가 맞는지?

--- src/main/java/com/study/SpringStudy/springmvc/interceptor/BoardInterceptor.java
+++ src/main/java/com/study/SpringStudy/springmvc/interceptor/BoardInterceptor.java
@@ -36,26 +36,28 @@ public class BoardInterceptor implements HandlerInterceptor {   //HandlerInterce
             return false;   //true일 경우, 컨트롤러 진입 허용 / false는 진입 차단
         }
 
-        //삭제 요청이 들어오면 서버에서 한번더 관리자인지? 자신이 쓴글인지 체크
+        //삭제 요청이 들어오면 서버에서 한번더 체크
+        // 관리자인지?
         if(LoginUtil.isAdmin(session)){
 
-            return true;
+            return true;  //통과
         }
-        //삭제 요청인지
+        //삭제 요청인지 (url 로 체크)
+        //    /board/delete?bno=33 처럼 들어와서 삭제됨
         if(redirectUri.equals("/board/delete")){
             //내가 쓴글인지
             // 현재 삭제하려는 글의 글쓴이 계정명과
-            //-> DB에서 조회해보면 됨 -> 글번호가 필요
+            //-> DB에서 조회해보면 됨 -> 글번호가 필요⭐️
             int bno = Integer.parseInt(request.getParameter("bno"));
-            Board board = boardMapper.findOne(bno);
-            String boardAccount = board.getAccount();
+            Board board = boardMapper.findOne(bno);     //  조회
+            String boardAccount = board.getAccount();    // 계정 가져오기
 
-            //현재 로그인한 회원의 계정명을 구해서
+            //현재 로그인한 회원의 계정명을 구해서 (세션에서)
             String loggedInUserAccount = LoginUtil.getLoggedInUserAccount(session);
 
             // 대조해보는 작업이 필요함
             if (!isMine(boardAccount, loggedInUserAccount)) {
-                response.setStatus(403);
+                response.setStatus(403);  //클라이언트가 서버에 접근하려고 했지만, 서버가 요청을 거부했다는 것
                 response.sendRedirect("/access-deny?message=authorization");
                 return false;
             }
